4c1998bda8ba8e97c81daa21e85559e4b9f0ca98
Minatallasabri <minatallasabri3380@gmail.com>
2023-11-02 14:56:25 -0700

    Task 3 first trying in python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        j = 1
+        while j <= n:
+            print("{:d}: {:s}".format(j, argv[j]))
+            j += 1
+
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
