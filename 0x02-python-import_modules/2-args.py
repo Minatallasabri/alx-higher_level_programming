42485e6cc8ce98899bc36c222a7da2ddac592d57
Minatallasabri <minatallasabri3380@gmail.com>
2023-11-02 14:58:09 -0700

    Task 3 first trying in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        j = 1
-        while j <= n:
-            print("{:d}: {:s}".format(j, argv[j]))
-            j += 1
-
 if __name__ == "__main__":
     import sys
-    print_arg(sys.argv)
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for arg in range(count):
+        print("{}: {}".format(arg + 1, sys.argv[arg + 1]))
