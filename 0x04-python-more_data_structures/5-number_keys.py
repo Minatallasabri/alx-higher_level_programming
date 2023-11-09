0472de6fe44e4f50f822089b95aba4e04b6f3e70
Minatallasabri <minatallasabri3380@gmail.com>
2023-11-08 17:38:24 -0800

    task num 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    numberkeys = 0
+    numkeys = 0
     for key in a_dictionary:
-        numberkeys += 1
-        return numberkeys
+        numkeys += 1
+        return numkeys
