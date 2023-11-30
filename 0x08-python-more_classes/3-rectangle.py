05e009a4a62a9d11049b5e2f9b9568a1813952e3
Minatallasabri <minatallasabri3380@gmail.com>
2023-11-29 17:05:03 -0800

    fix task 2, wtask 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,53 +1,61 @@
 #!/usr/bin/python3
-"""implement a new class"""
+"""
+New class Rectangle
+"""
 
 
 class Rectangle:
-    """Rectangle class"""
-    def __init__(self, width=0, height=0):
-        """init method
+    """ Defines a rectangle """
 
-        Args:
-            width (int): the width
-            height (int): the height
-        """
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
+    """ WIDTH """
     @property
     def width(self):
-        """Get/set the width
-        """
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    """ HEIGHT """
     @property
     def height(self):
-        """ Get/set the height"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    """ Rectangle area """
     def area(self):
-        """area method"""
         return (self.__height * self.__width)
 
+    """ Rectangle perimeter """
     def perimeter(self):
-        """ perimeter method
-        """
+        if self.width == 0:
+            return (0)
+        if self.height == 0:
+            return (0)
+        return (2 * (self.__height + self.__width))
+
+    """ Returns printable string representation
+    of an instance
+    """
+    def __str__(self):
+        empty_str = ""
+
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return ((self.__width * 2) + (self.__height * 2))
+            return (empty_str)
+
+        return ((('#' * self.width) + "\n") * self.height)[:-1]
