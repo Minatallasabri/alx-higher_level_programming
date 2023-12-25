b791e9d9b3f0b67eddf54c61fc06adbc4cfc11c5
Minatallasabri <minatallasabri3380@gmail.com>
2023-12-25 05:37:03 +0200

    try..task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-    """function return the list of available attributes
+"""function return the list of available attributes
     """
 
-    def lookup(obj):
-        """Return a list of an object's available attributes."""
-        return (dir(obj))
+
+def lookup(obj):
+    """the function
+
+    Args:
+        obj: the obj we want it attributes
+    """
+    return (dir(obj))
