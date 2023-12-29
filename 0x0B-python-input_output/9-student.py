d5f987fe457fa6eaaefc2dc51db87789ddb9e474
Minatallasabri <minatallasabri3380@gmail.com>
2023-12-29 04:18:14 +0200

    student-class task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Student to JSON """
+"""Declaration of Class"""
+
 
 class Student:
-    """ New class student """
-
-       def __init__(self, first_name, last_name, age):
+    """Class
+    """
+    def __init__(self, first_name, last_name, age):
         """init instance"""
         self.first_name = first_name
         self.last_name = last_name
