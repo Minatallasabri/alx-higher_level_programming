dbcc7294e584133076d0fe07e8586a40646c798a
Minatallasabri <minatallasabri3380@gmail.com>
2024-02-15 03:19:22 +0200

    fixing 2,3,5,7,8,9.10,12,13

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 if (isNaN(process.argv[2])) {
-    console.log('Missing size');
-  } else {
-    const times = parseInt(process.argv[2]);
-  
-    for (let i = 0; i < times; i++) {
-      console.log('X'.repeat(times));
-    }
+  console.log('Missing size');
+} else {
+  const times = parseInt(process.argv[2]);
+
+  for (let i = 0; i < times; i++) {
+    console.log('X'.repeat(times));
   }
-  
+}
