9493d4419f82d9908563f4b215ee1e1de08a5608
Minatallasabri <minatallasabri3380@gmail.com>
2024-02-15 02:41:27 +0200

    all mandatory tasks

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (isNaN(process.argv[2])) {
+    console.log('Missing size');
+  } else {
+    const times = parseInt(process.argv[2]);
+  
+    for (let i = 0; i < times; i++) {
+      console.log('X'.repeat(times));
+    }
+  }
