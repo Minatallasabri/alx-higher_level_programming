3b7dbc3cfb0e1c70b876075ea69e61ea1a84360c
Minatallasabri <minatallasabri3380@gmail.com>
2024-02-19 04:47:12 +0200

    again-fixingtask2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument."""
+"""Script that displays values in states table of hbtn_0e_0_usa
+   where name matches the argument
+"""
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
-    cursor = db.cursor()
-    cursor.execute(
-         '''SELECT * FROM states WHERE states.name = '{:s}' ORDER\
-        BY states.id ASC'''.format(sys.argv[4])
-    )
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
-
-    cursor.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
     db.close()
