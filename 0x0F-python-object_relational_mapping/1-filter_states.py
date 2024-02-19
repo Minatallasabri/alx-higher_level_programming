b0cea53889ee296e602d83dd99c0be147f197de5
Minatallasabri <minatallasabri3380@gmail.com>
2024-02-19 04:24:44 +0200

    fixing tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,9 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
-                         user=sys.argv[1], passwd=sys.argv[2])
-     cursor = db.cursor()
-     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+                        user=sys.argv[1], passwd=sys.argv[2])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     for row in c.fetchall():
         if row[1][0] in ['N']:
             print(row)
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Updates State object in the database hbtn_0e_6_usa
-"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+"""takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
-    states = session.query(State).order_by(State.id)
-    for state in states:
-        if state.id == 2:
-            state.name = 'New Mexico'
-    session.commit()
-    session.close()
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+        .format(sys.argv[4]))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
