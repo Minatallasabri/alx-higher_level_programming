d5de81281ac27debd3fcf51ae6a5f072e221815f
Minatallasabri <minatallasabri3380@gmail.com>
2024-02-19 04:51:56 +0200

    again_fixing_task12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument."""
-
-import MySQLdb
-import sys
-
+"""Updates State object in the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}'"
-        .format(sys.argv[4]))
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
-
-    cursor.close()
-    db.close()
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        if state.id == 2:
+            state.name = 'New Mexico'
+    session.commit()
