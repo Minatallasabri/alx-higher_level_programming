b82416f00ee5ab52bd6fb297b70a99cf936ef79f
Minatallasabri <minatallasabri3380@gmail.com>
2024-03-03 21:14:29 +0200

    fixing__task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""finds a peak in a list of unsorted integers."""
+"""Module for find_peak function"""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers."""
-    if list_of_integers is None or list_of_integers == []:
-        return None
-    return max(list_of_integers)
+    """A function that finds a peak in a list of unsorted integers"""
+    prev = 0
+    for idx, num in enumerate(list_of_integers):
+        if idx:
+            prev = list_of_integers[idx - 1]
+        if idx < len(list_of_integers) - 1:
+            next = list_of_integers[idx + 1]
+        else:
+            next = 0
+        if num >= prev and num >= next:
+            return num
