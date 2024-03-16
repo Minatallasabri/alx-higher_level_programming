8d839ce0a0ca8f5502b5770156d7620282ea2473
Minatallasabri <minatallasabri3380@gmail.com>
2024-03-16 16:17:10 +0200

    network#1 fixing**task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,13 @@
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
-import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
+if __name__ == '__main__':
+    import urllib.request
 
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(content.decode("utf8")))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
