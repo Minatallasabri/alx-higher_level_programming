2b38b492bc77ab0ac18f13e4610739b423c657a7
Minatallasabri <minatallasabri3380@gmail.com>
2024-03-16 16:00:19 +0200

    network#1 fixing_task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,3 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     print('\t- type: {}'.format(type(content)))
     print('\t- content: {}'.format(content))
     print('\t- utf8 content: {}'.format(content.decode("utf8")))
-    
